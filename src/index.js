aa53576d6d91df7337d7dd75225ce368d1df28f9
Abdulfatai <fob90s@gmail.com>
2021-11-08 16:43:56 +0100

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,39 +1,36 @@
 import './style.css';
 
-// class LeaderBoard {
-//   constructor() {
-//     this.memory = [];
-//     this.display();
-//   }
+/* class LeaderBoard {
+  constructor() {
+    this.memory = [];
+    this.display();
+  }
 
-//   add() {
-//     this.name = document.querySelector('input[type="text"]');
-//     this.score = document.querySelector('input[type="number"]');
-//     this.button = document.querySelector('form');
-//     console.log(this.button)
-//     this.button.addEventListener('submit', (event) => {
-//       console.log('addd');
-//       this.number = this.memory.length + 1;
-//       this.person = { name: 'Abdulfatai', score: 100, number: this.number };
-//       this.memory.push(this.person);
-//       console.log('ddsd');
-//       event.preventDefault();
-//     });
-//   }
+  add() {
+    this.name = document.querySelector('input[type="text"]');
+    this.score = document.querySelector('input[type="number"]');
+    this.button = document.querySelector('form');
+    this.button.addEventListener('submit', (event) => {
+      this.number = this.memory.length + 1;
+      this.person = { name: 'Abdulfatai', score: 100, number: this.number };
+      this.memory.push(this.person);
+      event.preventDefault();
+    });
+  }
 
-//   display() {
-//     this.root = document.querySelector('.root');
-//     this.fragment = new DocumentFragment();
-//     this.memory.forEach((currentItem, index) => {
-//       this.li = document.createElement('li');
-//       this.li.innerHTML = `
-//       <li id='${index + 1}'>${currentItem.name}: ${currentItem.score}</li>
-//       `;
-//       this.fragment.append(this.li);
-//     });
-//     this.root.append(this.fragment);
-//   }
-// }
+  display() {
+    this.root = document.querySelector('.root');
+    this.fragment = new DocumentFragment();
+    this.memory.forEach((currentItem, index) => {
+      this.li = document.createElement('li');
+      this.li.innerHTML = `
+      <li id='${index + 1}'>${currentItem.name}: ${currentItem.score}</li>;
+      `;
+      this.fragment.append(this.li);
+    });
+    this.root.append(this.fragment);
+  }
+}
 
-// const leaderBoard = new LeaderBoard();
-// leaderBoard.add();
+const leaderBoard = new LeaderBoard();
+leaderBoard.add();  */
