bacf93d25e6781ce4b31528047113f8d643dd14e
Abdulfatai <fob90s@gmail.com>
2021-11-08 18:17:02 +0100

    Remove comment and update code

--- src/index.js
+++ src/index.js
@@ -1,36 +1,27 @@
 import './style.css';
 
-/* class LeaderBoard {
+class LeaderBoard {
   constructor() {
-    this.memory = [];
+    this.memory = [
+      { name: 'Abdulfatai', score: 100 },
+      { name: 'Raymond', score: 90 },
+    ];
     this.display();
-  }
-
-  add() {
-    this.name = document.querySelector('input[type="text"]');
-    this.score = document.querySelector('input[type="number"]');
-    this.button = document.querySelector('form');
-    this.button.addEventListener('submit', (event) => {
-      this.number = this.memory.length + 1;
-      this.person = { name: 'Abdulfatai', score: 100, number: this.number };
-      this.memory.push(this.person);
-      event.preventDefault();
-    });
   }
 
   display() {
     this.root = document.querySelector('.root');
     this.fragment = new DocumentFragment();
     this.memory.forEach((currentItem, index) => {
-      this.li = document.createElement('li');
-      this.li.innerHTML = `
-      <li id='${index + 1}'>${currentItem.name}: ${currentItem.score}</li>;
+      this.ul = document.createElement('ul');
+      this.ul.innerHTML = `
+      <li id='${index + 1}'>${currentItem.name}: ${currentItem.score}</li>
       `;
-      this.fragment.append(this.li);
+      this.fragment.append(this.ul);
     });
     this.root.append(this.fragment);
   }
 }
 
+// eslint-disable-next-line no-unused-vars
 const leaderBoard = new LeaderBoard();
-leaderBoard.add();  */
