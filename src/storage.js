973bc9eb4fe63089d59513cb40fa5c27321955ab
Abdulfatai <fob90s@gmail.com>
2021-11-12 11:41:16 +0100

    Remove console.log and unnecessary comments

--- src/storage.js
+++ src/storage.js
@@ -1,8 +1,7 @@
 import { listenToEvent, callApi } from './use.js';
 
-const gameId = 'vhzB1Yi0ThkTiPdCiOHz';
+const gameId = 'vfo3jpl5RXpVbquV5PS0';
 const url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores/`;
-const ulContainer = document.createElement('ul');
 
 const getInputValues = () => {
   let user;
@@ -11,54 +10,36 @@ const getInputValues = () => {
   input.forEach((currentItem) => {
     if (currentItem.name === 'name') {
       user = currentItem.value;
+    } else {
+      score = currentItem.value;
     }
   });
   return (score) ? JSON.stringify({ user, score }) : '';
 };
 
-// const scores = getInputValues();
-
 const postScores = (api, listen, link, getInputValue) => {
+  let result;
   listen('click', async (event) => {
     const scores = getInputValue();
     if (event) {
       const { action } = event.target.dataset;
-      if (action === 'postScores') {
-        const res = await api('POST', link, scores);
-        console.log(res);
+      if (action === 'submit') {
+        result = await api('POST', link, scores);
       }
     }
+    return result;
   });
 };
 postScores(callApi, listenToEvent, url, getInputValues);
 
-
-// const handler = () => {
-// postScores(callApi, listenToEvent, url, scores);
-// }
-
-// handler()
-
 const getScores = async (api, link) => {
   const method = 'GET';
   const result = await api(method, link);
-  // console.log(result);
   return result;
 };
 
 getScores(callApi, url);
 
 export {
-  ulContainer, url, getInputValues, postScores, getScores,
+  url, getScores,
 };
-
-// const create = async () => {
-//   await callApi('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', {
-//     method: 'POST',
-//     body: JSON.stringify({ name: 'Fobadara\'s Cool Game' }),
-//     headers: {
-//       'Content-type': 'application/json',
-//     },
-//   }).then((response) => response.json()).then((json) => console.log(json));
-// };
-// create();
